4b3c45e5de8b553d2015650b7298db02a5a14cf1
lavanya2001-lavi <lavilavanya384@gmail.com>
2026-05-13 13:53:48 +0530

    Updated superbase url

--- src/app/api/booking/route.js
+++ src/app/api/booking/route.js
@@ -2,6 +2,8 @@ import { NextResponse } from 'next/server';
 import { sendDetailedBookingAdminMail, sendDetailedBookingUserMail } from '@/app/services/emailService';
 import { supabase } from '@/lib/supabase';
 
+export const dynamic = 'force-dynamic';
+
 export async function GET(request) {
   try {
     const { searchParams } = new URL(request.url);
